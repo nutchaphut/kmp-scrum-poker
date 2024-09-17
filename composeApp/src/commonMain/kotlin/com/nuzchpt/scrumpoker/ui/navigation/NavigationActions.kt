876e80ca164c49c3ac6f31dcadc4cd0537ab8ab9
nutchaphut <nutchaphut@nocnoc.com>
2024-09-17 16:04:59 +0700

    [Feat] migrate to navigation 2.8.0

--- composeApp/src/commonMain/kotlin/com/nuzchpt/scrumpoker/ui/navigation/NavigationActions.kt
+++ composeApp/src/commonMain/kotlin/com/nuzchpt/scrumpoker/ui/navigation/NavigationActions.kt
@@ -2,39 +2,28 @@ package com.nuzchpt.scrumpoker.ui.navigation
 
 import androidx.navigation.NavController
 import androidx.navigation.navOptions
-import com.nuzchpt.scrumpoker.ui.navigation.NavigationDestinations.MAIN_ROUTE
-import com.nuzchpt.scrumpoker.ui.navigation.NavigationDestinations.ROOM_ROUTE
-import com.nuzchpt.scrumpoker.ui.navigation.Screen.MAIN
-import com.nuzchpt.scrumpoker.ui.navigation.Screen.PROFILE
-import com.nuzchpt.scrumpoker.ui.navigation.Screen.ROOM
-
-object Screen {
-    const val MAIN = "MAIN"
-    const val PROFILE = "EDIT_PROFILE"
-    const val ROOM = "ROOM"
-}
-
-object NavigationDestinations {
-    const val MAIN_ROUTE = MAIN
-    const val PROFILE_ROUTE = PROFILE
-    const val ROOM_ROUTE = "$ROOM?$ARGS={$ARGS}"
-}
+import kotlinx.serialization.Serializable
 
 class NavigationActions(private val navController: NavController) {
 
     fun navigateToRoom(roomId: String) {
-        navController.navigate(ROOM_ROUTE.replace("{$ARGS}", roomId),
+        navController.navigate(ScreenRoom(roomId = roomId),
             navOptions = navOptions {
                 launchSingleTop = true
-            })
+            }
+        )
     }
 
     fun navigateToMain() {
-        navController.navigate(MAIN_ROUTE, navOptions = navOptions {
+        navController.navigate(ScreenMain, navOptions = navOptions {
             launchSingleTop = true
         })
     }
 
 }
 
-const val ARGS = "args"
+@Serializable
+object ScreenMain
+
+@Serializable
+data class ScreenRoom(val roomId: String)
